81f44312d69b73e402cafdeb434ca375300cf25e
jacobis <remind.jacob@gmail.com>
2013-12-10 16:49:46 +0900

    Admin user or read only permission added

--- couple_mission/libs/permissions.py
+++ couple_mission/libs/permissions.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from rest_framework import permissions
 
 from couple_mission.apps.couple.controller import CoupleController
+
+
+SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
 
 
 class IsOwnerOrCoupleOnly(permissions.BasePermission):
@@ -14,3 +19,19 @@ class IsOwnerOrCoupleOnly(permissions.BasePermission):
         print is_couple
 
         return is_owner or is_couple
+
+
+class IsAdminUserOrReadOnly(permissions.BasePermission):
+
+    """
+    Admin 유저가 아닐 경우, Read만 허용
+    """
+
+    def has_permission(self, request, view):
+        print request.user
+        print request.user.is_staff
+        if request.method in SAFE_METHODS:
+            return True
+        if request.user and request.user.is_staff:
+            return True
+        return False
