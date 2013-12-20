686c374b4a9c6f5d458c08d9c7a0e48e375b0686
jacobis <remind.jacob@gmail.com>
2013-12-20 13:49:24 +0900

    Notice list view modified

--- couple_mission/apps/headquarter/views.py
+++ couple_mission/apps/headquarter/views.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext as _
 # REST Framework
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import status
 
 # Permissions
 from couple_mission.libs.permissions import IsAdminUserOrReadOnly
@@ -22,3 +23,9 @@ class NoticeViewSet(viewsets.ModelViewSet):
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
     permission_classes = (IsAdminUserOrReadOnly,)
+
+    def list(self, request, *args, **kwargs):
+        self.object_list = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(self.object_list, many=True)
+
+        return Response({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
